32371da0d7f0bd0785e33d0961b8c40f8e638152
Ankit Roy <ankitkumar98roy@gmail.com>
2024-06-24 10:16:10 +0530

    lec17

---
+++ summerTranning.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int add(int a){
+	 
+	 int b=7;
+	 cout<<b<<endl;
+	cout<<a+b<<endl;
+}
+int main(){
+	int a=5;
+	int b=56;
+	add(a);
+	cout<<b<<endl;
+	
+	
+}
